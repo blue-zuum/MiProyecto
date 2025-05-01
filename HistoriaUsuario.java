ae2d61fd73913146fd078c4a37e2f20d3390dc64
luz <luzmercercedes2017@gmail.com>
2025-05-01 18:08:00 -0500

    Primer comentario holi

---
+++ HistoriaUsuario.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sobrecarga;
+
+/**
+ *
+ * @author PC
+ */
+class HistoriaUsuario {
+    String id, rol, funcionalidad, razon, criterio, contexto, evento, resultadoEsperado;
+
+    public HistoriaUsuario(String id, String rol, String funcionalidad, String razon,
+                           String criterio, String contexto, String evento, String resultadoEsperado) {
+        this.id = id;
+        this.rol = rol;
+        this.funcionalidad = funcionalidad;
+        this.razon = razon;
+        this.criterio = criterio;
+        this.contexto = contexto;
+        this.evento = evento;
+        this.resultadoEsperado = resultadoEsperado;
+    }
+
+    public void imprimir() {
+        System.out.println("ID: " + id);
+        System.out.println("Rol: " + rol);
+        System.out.println("Funcionalidad: " + funcionalidad);
+        System.out.println("Razón: " + razon);
+        System.out.println("Criterio: " + criterio);
+        System.out.println("Contexto: " + contexto);
+        System.out.println("Evento: " + evento);
+        System.out.println("Resultado esperado: " + resultadoEsperado);
+        System.out.println("--------------------------");
+    }
+}
